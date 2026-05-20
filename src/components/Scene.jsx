767a79c540ce0ca6b209cc91c2f2ce9084f68996
Minh Vu <ngominhvu2003@gmail.com>
2026-05-20 15:01:08 +0700

    Adjust loading screen and scene responsiveness display on different devices

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -3,9 +3,11 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Environment, Center } from '@react-three/drei'
 import { OldTv } from './OldTv'
 
+const isMobile = window.matchMedia('(max-width: 768px)').matches
+
 export function Scene() {
   return (
-    <Canvas camera={{ position: [0, 0, 15], fov: 50 }}>
+    <Canvas camera={{ position: [0, 0, isMobile ? 22 : 13], fov: 50 }}>
       <ambientLight intensity={0.4} />
       <directionalLight position={[5, 5, 5]} intensity={1} />
       <Suspense fallback={null}>
